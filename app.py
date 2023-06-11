fecb819e291f23aa37655364b750ad0dbdafd0c1
thebadsektor <thebadsektor@gmail.com>
2023-06-12 01:08:19 +0800

    edits

--- app.py
+++ app.py
@@ -55,5 +55,10 @@ response_container = st.container()
 # container for text box
 container = st.container()
 
-if clear_button:
-    respond()
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
+
+        if submit_button:
+            respond()
