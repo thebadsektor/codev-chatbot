7c0582e75eed77f8bbfd9202bfb8fb8db296c3ba
thebadsektor <thebadsektor@gmail.com>
2023-06-12 00:56:38 +0800

    adding ui

--- app.py
+++ app.py
@@ -16,14 +16,44 @@ else:
 
 # API key is set as an environment variable
 df = pd.read_csv('data/knowledge-base.csv')
-print(df[0:10])
+# print(df[0:10])
 
 csv_path = 'data/knowledge-base.csv'
 
 csv_agent = create_csv_agent(OpenAI(temperature=0), csv_path, verbose=True)
-# output = csv_agent.run("Look for similar questions in the intent column and find the answer from the response column. Here is the question: What is LSPU?")
-# print(output)
+question = "Look for similar questions in the intent column and find the answer from the response column. Here is the question: What is LSPU?"
+
+def respond():
+    output = csv_agent.run(question)
+    print(output)
+
+# # Initialise session state variables
+# if 'generated' not in st.session_state:
+#     st.session_state['generated'] = []
+# if 'past' not in st.session_state:
+#     st.session_state['past'] = []
+# if 'messages' not in st.session_state:
+#     st.session_state['messages'] = [
+#         {"role": "system", "content": "You are a helpful assistant."}
+#     ]
 
 # Setting page title and header
 st.set_page_config(page_title="AVA", page_icon=":robot_face:")
-st.markdown("<h1 style='text-align: center;'>AVA - a totally harmless chatbot 😬</h1>", unsafe_allow_html=True)
+st.markdown(
+    "<h1 style='text-align: center;'>CoDev Chatbot</h1>"
+    + "<p style='text-align: center;'>Role: University Assistant</p>", 
+    unsafe_allow_html=True)
+
+st.sidebar.title("Welcome")
+counter_placeholder = st.sidebar.empty()
+counter_placeholder.write(f"Gerald Villaran<br>Chatbot")
+q_button = st.sidebar.button("Test", key="test")
+clear_button = st.sidebar.button("Clear Conversation", key="clear")
+
+# container for chat history
+response_container = st.container()
+# container for text box
+container = st.container()
+
+if clear_button:
+    respond()
