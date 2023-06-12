f204c0b4b2e1f4ca8fe5af4b622948ccc682298a
thebadsektor <thebadsektor@gmail.com>
2023-06-12 10:14:13 +0800

    respond function

--- app.py
+++ app.py
@@ -22,23 +22,22 @@ csv_path = 'data/knowledge-base.csv'
 
 csv_agent = create_csv_agent(OpenAI(temperature=0), csv_path, verbose=True)
 question = "Look for similar questions in the intent column and find the answer from the response column. Here is the question: What is LSPU?"
+    
 
-def respond():
-    output = csv_agent.run(question)
-    print(output)
-
-# # Initialise session state variables
-# if 'generated' not in st.session_state:
-#     st.session_state['generated'] = []
-# if 'past' not in st.session_state:
-#     st.session_state['past'] = []
-# if 'messages' not in st.session_state:
-#     st.session_state['messages'] = [
-#         {"role": "system", "content": "You are a helpful assistant."}
-#     ]
+# Initialise session state variables.
+# Session State is a way to share variables between reruns, for each user session. 
+# In addition to the ability to store and persist state.
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
 
 # Setting page title and header
-st.set_page_config(page_title="AVA", page_icon=":robot_face:")
+st.set_page_config(page_title="", page_icon=":robot_face:")
 st.markdown(
     "<h1 style='text-align: center;'>CoDev Chatbot</h1>"
     + "<p style='text-align: center;'>Role: University Assistant</p>", 
@@ -46,7 +45,7 @@ st.markdown(
 
 st.sidebar.title("Welcome")
 counter_placeholder = st.sidebar.empty()
-counter_placeholder.write(f"Gerald Villaran<br>Chatbot")
+counter_placeholder.write(f"Gerald Villaran")
 q_button = st.sidebar.button("Test", key="test")
 clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
@@ -60,5 +59,21 @@ with container:
         user_input = st.text_area("You:", key='input', height=100)
         submit_button = st.form_submit_button(label='Send')
 
-        if submit_button:
-            respond()
+    if submit_button and user_input:
+        output = csv_agent.run("Look for similar questions in the intent column and find the answer from the response column. Here is the question:" + user_input)
+        # output = csv_agent.run("Look for similar questions in the intent column and find the answer from the response column. Here is the question:" + user_input + ". If the question is not related to 'Enrollment' just say 'Sorry.' ")
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+
+if q_button:
+    p = "Categorize the whole dataframe into topics. put total number of questions per category. use practical vocabulary. use bulleted list."
+    output = csv_agent.run(p)
+    st.session_state['past'].append(p)
+    st.session_state['generated'].append(output)
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
+
